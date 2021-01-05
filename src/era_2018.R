32c03118494cb98fd7a7c3b9faf7eebc02325e19
7070707070 <sky2502211@gmail.com>
2021-01-05 22:00:06 +0800

    Add 2018,2019 era-win picture

---
+++ src/era_2018.R
@@ -0,0 +1,16 @@
+library(readr)
+source_data_2019 <- read.table("DATA/2019.txt")
+source_data_2018 <- read.table("DATA/2018.txt")
+
+era_2018 <- vector()
+wins_2018 <- vector()
+
+for (i in c(1:30)){
+    wins_2018 <- c(wins_2018,source_data_2018[i,2])    
+    era_2018  <- c(era_2018,source_data_2018[i,4])
+}
+
+
+png(file = "PICTURE/2018.png")
+plot(era_2018, wins_2018)
+dev.off()
---
+++ src/era_2019.R
@@ -0,0 +1,15 @@
+library(readr)
+source_data_2019 <- read.table("DATA/2019.txt")
+source_data_2018 <- read.table("DATA/2018.txt")
+
+era_2019 <- vector()
+wins_2019 <- vector()
+
+for (i in c(1:30)){
+    wins_2019 <- c(wins_2019,source_data_2019[i,2])    
+    era_2019  <- c(era_2019,source_data_2019[i,4])
+}
+
+png(file = "PICTURE/2019.png")
+plot(era_2019, wins_2019)
+dev.off()
